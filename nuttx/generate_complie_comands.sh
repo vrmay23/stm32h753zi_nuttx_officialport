72cd8f457487e6d194d942cc7795f0d75448abf0
vrmay23 <vmay.sweden@gmail.com>
2026-02-21 09:43:11 +0100

    infra: sync vscode settings and update car_can DBC

---
+++ nuttx/generate_complie_comands.sh
@@ -0,0 +1,5 @@
+make --always-make --dry-run \
+ | grep -wE 'gcc|g\+\+' \
+ | grep -w '\-c' \
+ | jq -nR '[inputs|{directory:".", command:., file: match(" [^ ]+$").string[1:]}]' \
+ > compile_commands.json
